e732d850458d310b9acc81bb57feb34822def05b
Shaundator <rash0081@kea.stud.dk>
2022-11-23 11:39:18 +0100

    Navbar made fraction

---
+++ app.js
@@ -0,0 +1,17 @@
+function start(){
+    document.querySelector("#navbarNav").innerHTML = 
+    '<ul class="navbar-nav">\
+    <li class="nav-item">\
+      <a class="nav-link active" aria-current="page" href="index.html">Forside</a>\
+    </li>\
+    <li class="nav-item">\
+      <a class="nav-link active" href="tidsbestilling.html">Tidsbestilling</a>\
+    </li>\
+    <li class="nav-item">\
+      <a class="nav-link active" href="priser.html">Priser</a>\
+    </li>\
+    <li class="nav-item">\
+      <a class="nav-link active" href="galleri.html">Galleri</a>\
+    </li>\
+    </ul>';
+}
