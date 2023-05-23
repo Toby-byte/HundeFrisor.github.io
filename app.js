f71994b0ac30f85f6b46b417abaa5aed589cda6d
Toby-byte <tobiasbogenielsen@gmail.com>
2023-05-23 15:33:24 +0200

    reduced and fixed

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ function start(){
       <a class="nav-link active" aria-current="page" href="index.html">Forside</a>\
     </li>\
     <li class="nav-item">\
-      <a class="nav-link active" href="tidsbestilling.html">Tidsbestilling</a>\
-    </li>\
-    <li class="nav-item">\
       <a class="nav-link active" href="priser.html">Priser</a>\
-    </li>\
-    <li class="nav-item">\
-      <a class="nav-link active" href="galleri.html">Galleri</a>\
     </li>\
     </ul>';
 }
